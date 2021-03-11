9aa477047cdf7cfb8089620834fc034032727de1
Melih <61734833+melih-oss@users.noreply.github.com>
2021-03-11 10:10:53 +0300

    Add files via upload

---
+++ ConsoleApp2/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            
+            string a = "melihakman07@hotmail.com";
+            char[] b = new char[12];
+            int i;
+            for(i = 0; a[i] !=  '@' ; i++)
+            {
+                b[i] = a[i];
+            }
+            Console.WriteLine(i);
+            Console.WriteLine(b);
+            Console.Read();
+        }
+    }
+}
